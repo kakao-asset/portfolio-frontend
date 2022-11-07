8c43db8a444102e5fa9b9a5ae876df7a4d27a34a
21ONG <efrien489@gmail.com>
2022-11-07 16:01:57 +0900

    검색창 수정

--- src/component/mainpage/header/Searchbar.js
+++ src/component/mainpage/header/Searchbar.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import SearchModal from "./SearchModal";
 import Modal from 'react-modal';
+import { BsSearch } from "react-icons/bs";
 
 export default function Searchbar() {
     const [search, setSearch] = useState("");
@@ -11,7 +12,7 @@ export default function Searchbar() {
 
     return (
         <div>
-            <input type="text" value={search} onChange={{onChange}} onClick={()=> setModalIsOpen(true)} style={{
+            <input type="text" onClick={()=> setModalIsOpen(true)} style={{
                 width: '400px',
                 height: '30px',
                 backgroundColor: '#1F1F1F',
@@ -29,9 +30,40 @@ export default function Searchbar() {
                 style={{
                     overlay: {
                         position: 'fixed',
-                        backgroundColor: '#1F1F1F'
+                        backgroundColor: 'rgba(255, 255, 255, 0)'
                     },
-                }}>검색 모달 창</Modal>
+                    content: {
+                        position: 'relative',
+                        top: '40px',
+                        left: '1100px',
+                        overflow: 'auto',
+                        borderRadius: '4px',
+                        width: '600px',
+                        height: '700px',
+                        background: '#1F1F1F'
+
+                    }
+                }}>
+                    <div style={{}}>
+                    <input id="searchText" name="searchText" type="text" onChange={onChange} style={{
+                width: '600px',
+                height: '30px',
+                backgroundColor: '#1F1F1F',
+                border: '1px solid #D2D2D2',
+                background: 'no-repeat scroll 1px 1px',
+                backgroundSize: 'contain',
+                backgroundPosition: 'left',
+                position: 'absolute',
+                color: 'white',
+                top: '5px',
+                left: '10px',
+                right: '10px'        
+                }}></input>
+                    </div>
+                    <div style={{marginTop: '40px'}}>
+                        <p1 style={{color: 'white'}}>{search}</p1>
+                    </div>
+                </Modal>
         </div>
     );
 }
