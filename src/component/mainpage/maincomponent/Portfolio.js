347cd2b465a6039408eaee4e58e1b6a3da80b8e8
21ONG <efrien489@gmail.com>
2022-11-15 03:39:28 +0900

    검색 상세 페이지 보이는 정보 추가

--- src/component/mainpage/maincomponent/Portfolio.js
+++ src/component/mainpage/maincomponent/Portfolio.js
@@ -31,7 +31,7 @@ export default function Portfolio({stockHold}){
                 <div style={{display: "flex"}}>
                     {/* 총 자산은 현재 보유 주식 실시간 데이터의 현재가 X 보유 개수 */}
                     {/* 지금은 보유 주식 평단가 X 보유 개수로 계산 */}
-                    <p style={{marginLeft: '30px', color: "white", paddingTop: "53px"}}>총 자산</p>
+                    <p style={{marginLeft: '30px', color: "white", paddingTop: "37px"}}>총 자산</p>
                     <h3 style={{marginLeft: '5%', color: "white", paddingTop: "30px", paddingBottom: "30px"}}>{totalAsset.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}원</h3>
                 </div>
             </div>
--- src/component/searchpage/InfoList.js
+++ src/component/searchpage/InfoList.js
@@ -10,6 +10,7 @@ export default function InfoList  ({stockInfo})  {
         const lowPrice = stockInfo.lowPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
         return (
+            <div style={{paddingBottom: '100px'}}>
             <div style={{marginTop: '40px', marginBotton: '20px', display: 'flex'}}>
                 <div style={{marginLeft: '30px'}}>
                     <span style={{color: 'white', paddingLeft: '20px', display: 'block', width: '100px', opacity: '0.7'}}>전일 종가</span>
@@ -32,6 +33,50 @@ export default function InfoList  ({stockInfo})  {
                     <span style={{paddingLeft: '20px', display: 'block', fontSize: '20px', color: '#E35277'}}>{lowPrice}원</span>
                 </div>
              </div>
+             <div style={{marginTop: '40px', marginBotton: '20px', display: 'flex'}}>
+                <div style={{marginLeft: '30px'}}>
+                    <span style={{color: 'white', paddingLeft: '20px', display: 'block', width: '100px', opacity: '0.7'}}>거래대금</span>
+                    <span style={{color: 'white', paddingLeft: '20px', display: 'block', fontSize: '20px'}}>{stockInfo.accTradeVolume.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}원</span>
+                </div>
+                <div style={{marginLeft: '30px'}}>
+                    <span style={{color: 'white', paddingLeft: '20px', display: 'block', width: '100px', opacity: '0.7'}}>상한가</span>
+                    <span style={{color: 'white', paddingLeft: '20px', display: 'block', fontSize: '20px'}}>{stockInfo.upperLimitPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}원</span>
+                </div>
+                <div style={{marginLeft: '30px'}}>
+                    <span style={{color: 'white', paddingLeft: '20px', display: 'block', width: '100px', opacity: '0.7'}}>하한가</span>
+                    <span style={{color: 'white', paddingLeft: '20px', display: 'block', fontSize: '20px'}}>{stockInfo.lowerLimitPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}원</span>
+                </div>
+                <div style={{marginLeft: '30px'}}>
+                    <span style={{color: 'white', paddingLeft: '20px', display: 'block', width: '100px', opacity: '0.7'}}>52주 최고가</span>
+                    <span style={{paddingLeft: '20px', display: 'block', fontSize: '20px', color: 'white'}}>{stockInfo.high52wPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}원</span>
+                </div>
+                <div style={{marginLeft: '30px'}}>
+                    <span style={{color: 'white', paddingLeft: '20px', display: 'block', width: '100px', opacity: '0.7'}}>52주 최저가</span>
+                    <span style={{paddingLeft: '20px', display: 'block', fontSize: '20px', color: 'white'}}>{stockInfo.low52wPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}원</span>
+                </div>
+                <div style={{marginLeft: '30px'}}>
+                    <span style={{color: 'white', paddingLeft: '20px', display: 'block', width: '100px', opacity: '0.7'}}>외국인 비율</span>
+                    <span style={{color: 'white', paddingLeft: '20px', display: 'block', fontSize: '20px'}}>{stockInfo.foreignRatio}%</span>
+                </div>
+                <div style={{marginLeft: '30px'}}>
+                    <span style={{color: 'white', paddingLeft: '20px', display: 'block', width: '100px', opacity: '0.7'}}>시가총액</span>
+                    <span style={{color: 'white', paddingLeft: '20px', display: 'block', fontSize: '20px'}}>{stockInfo.marketCap.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}원</span>
+                </div>
+                <div style={{marginLeft: '30px'}}>
+                    <span style={{color: 'white', paddingLeft: '20px', display: 'block', width: '100px', opacity: '0.7'}}>EPS/PER</span>
+                    <span style={{paddingLeft: '20px', display: 'block', fontSize: '20px', color: 'white'}}>{stockInfo.eps}/{stockInfo.per}</span>
+                </div>
+                <div style={{marginLeft: '30px'}}>
+                    <span style={{color: 'white', paddingLeft: '20px', display: 'block', width: '100px', opacity: '0.7'}}>BPS/PBR</span>
+                    <span style={{paddingLeft: '20px', display: 'block', fontSize: '20px', color: 'white'}}>{stockInfo.bps}/{stockInfo.pbr}</span>
+                </div>
+                <div style={{marginLeft: '30px'}}>
+                    <span style={{color: 'white', paddingLeft: '20px', display: 'block', width: '100px', opacity: '0.7'}}>업종</span>
+                    <span style={{color: 'white', paddingLeft: '20px', display: 'block', fontSize: '20px'}}>{stockInfo.sectorName}</span>
+                </div>
+             </div>
+            </div>
+
 
 
         );
