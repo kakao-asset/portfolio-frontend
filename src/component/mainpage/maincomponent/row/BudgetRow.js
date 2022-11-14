eaac3965455a123e2561ae143777634e2769947e
21ONG <efrien489@gmail.com>
2022-11-15 00:07:47 +0900

    줄맞춤 수정

--- src/component/mainpage/maincomponent/row/BudgetRow.js
+++ src/component/mainpage/maincomponent/row/BudgetRow.js
@@ -116,7 +116,7 @@ import axios from "axios";
                 <div style={{display: 'flex', marginLeft: '30px'}}>
                     <BsCart4 size='20px' style={{color: '#D9D9D9', display: 'block', paddingTop: '12px'}}></BsCart4>
                     <span style={{color: 'white', paddingLeft: '30px', display: 'block', width: '150px', paddingTop: '10px'}}>{stockName}</span>
-                    <span style={{color: 'white', paddingLeft: '100px', display: 'block', paddingTop: '10px', width: '90px'}}>{stockAvgPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}원</span>
+                    <span style={{color: 'white', paddingLeft: '100px', display: 'block', paddingTop: '10px', width: '90px', textAlign: 'right'}}>{stockAvgPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}원</span>
                     <button className={styles.sellButton} onClick={()=>setSellPopIsOpen(true)}>매도</button>
                     <button className={styles.buyButton} onClick={()=>setBuyPopIsOpen(true)}>매수</button>
 
