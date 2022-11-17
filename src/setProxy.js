ccb1ec181cee23f9ffcc68db9cf55579d8a70f61
yn15 <68692931+yn15@users.noreply.github.com>
2022-11-17 11:21:51 +0900

    middleware

--- src/setProxy.js
+++ src/setProxy.js
@@ -1,39 +1,39 @@
-// src/setupProxy.js
-const { createProxyMiddleware } = require('http-proxy-middleware');
-const BACK_URI = process.env.REACT_APP_BACKEND_URI
-const FRONT_URI = process.env.REACT_APP_FRONT_URI
-module.exports = function(app) {
+// // src/setupProxy.js
+// const { createProxyMiddleware } = require('http-proxy-middleware');
+// const BACK_URI = process.env.REACT_APP_BACKEND_URI
+// const FRONT_URI = process.env.REACT_APP_FRONT_URI
+// module.exports = function(app) {
 
-  //CORS ERROR
-    app.use(
-        '/api',
-        createProxyMiddleware({
-          target: BACK_URI,
-          changeOrigin: true,
-        })
-    );
+//   //CORS ERROR
+//     app.use(
+//         '/api',
+//         createProxyMiddleware({
+//           target: BACK_URI,
+//           changeOrigin: true,
+//         })
+//     );
       
-    app.use(
-        '/main',
-        createProxyMiddleware({
-            target: BACK_URI,
-            changeOrigin: true,
-        })
-    );
+//     app.use(
+//         '/main',
+//         createProxyMiddleware({
+//             target: BACK_URI,
+//             changeOrigin: true,
+//         })
+//     );
 
-    app.use(
-        '/search',
-        createProxyMiddleware({
-            target: BACK_URI,
-            changeOrigin: true,
-        })
-    );
+//     app.use(
+//         '/search',
+//         createProxyMiddleware({
+//             target: BACK_URI,
+//             changeOrigin: true,
+//         })
+//     );
 
-    app.use(
-        '/sector',
-        createProxyMiddleware({
-            target: BACK_URI,
-            changeOrigin: true,
-        })
-    );
-};
+//     app.use(
+//         '/sector',
+//         createProxyMiddleware({
+//             target: BACK_URI,
+//             changeOrigin: true,
+//         })
+//     );
+// };
