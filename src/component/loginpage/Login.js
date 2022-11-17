b81ef511924a330e50adf03467196df6c24ae98c
21ONG <efrien489@gmail.com>
2022-11-17 18:27:55 +0900

    로그인 페이지 레이아웃 수정 중

--- src/component/loginpage/Login.js
+++ src/component/loginpage/Login.js
@@ -15,8 +15,8 @@ export default function Login() {
         <div style={{textAlign: "center"}}>
             <div style={{display: 'flex'}}>
                 <div className={styles.leftBox}>
-                    <img className="kaLogo" src="img/ka_logo_s.png" alt="kaLogo" style={
-                        {paddingTop: '15%'}}></img>
+                    <img className="kaLogo" src="img/ka_logo_kaasset.png" alt="kaLogo" style={
+                        {paddingTop: '15%', width: '310px'}}></img>
                     
                     <img src="img/ka_login_page_img.png" alt="ka_login_page_img_1"
                     style={{width: '850px'}}
@@ -31,6 +31,18 @@ export default function Login() {
                     <div>
                     <KakaoLoginButton/>
                     </div>
+                    <button 
+                    style={{marginTop: '290px', 
+                    marginLeft: '550px', 
+                    color: 'black',
+                    backgroundColor: 'white',
+                    border: '0px',
+                    paddingTop: '10px',
+                    paddingBottom: '12px',
+                    paddingLeft: '15px',
+                    paddingRight: '15px',
+                    borderRadius: '5px'}}>
+                        관리자 로그인</button>
                 </div>
             </div>
         </div>
