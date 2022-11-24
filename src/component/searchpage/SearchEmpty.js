1e3a4d22d422c4f401c5ae4610052df9799c4b2a
21ONG <efrien489@gmail.com>
2022-11-24 15:14:58 +0900

    검색 데이터 로딩 화면 추가

---
+++ src/component/searchpage/SearchEmpty.js
@@ -0,0 +1,11 @@
+import Header from "../mainpage/header/Header";
+
+export default function SearchEmpty() {
+    return(
+       
+            <div>
+                <p style={{color: 'white', paddingTop: '5rem'}}>검색 데이터를 찾고 있어요.</p>
+                <img alt="empty image" src="img/ka_search_loading.png"></img>
+            </div>
+    );    
+};
